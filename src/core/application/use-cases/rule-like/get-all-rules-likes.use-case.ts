6e03a60fa7621462e9a3e3ceb873eddbdc6ef9d7
malinosqui <gabrielmalinosqui@gmail.com>
2025-04-30 14:20:10 -0300

    feat: add return type for GetAllRuleLikesUseCase execute method

--- src/core/application/use-cases/rule-like/get-all-rules-likes.use-case.ts
+++ src/core/application/use-cases/rule-like/get-all-rules-likes.use-case.ts
@@ -3,6 +3,7 @@ import {
     IRuleLikeService,
     RULE_LIKE_SERVICE_TOKEN,
 } from '@/core/domain/kodyRules/contracts/ruleLike.service.contract';
+import { RuleLikeEntity } from '@/core/domain/kodyRules/entities/ruleLike.entity';
 
 @Injectable()
 export class GetAllRuleLikesUseCase {
@@ -11,7 +12,7 @@ export class GetAllRuleLikesUseCase {
         private readonly ruleLikeService: IRuleLikeService,
     ) {}
 
-    async execute() {
+    async execute(): Promise<RuleLikeEntity[]> {
         return this.ruleLikeService.getAllLikes();
     }
 }
