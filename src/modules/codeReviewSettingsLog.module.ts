9793150f3cd8ce7a35dda5ea4c36a3b0bb257cbd
Junior Sartori <sartorijr92@gmail.com>
2025-07-21 16:43:10 -0300

    Adding module and references

---
+++ src/modules/codeReviewSettingsLog.module.ts
@@ -0,0 +1,26 @@
+import { CodeReviewSettingsLogRepository } from '@/core/infrastructure/adapters/repositories/mongoose/codeReviewSettingsLog.repository';
+import { CodeReviewSettingsLogModelInstance } from '@/core/infrastructure/adapters/repositories/mongoose/schema';
+import { CodeReviewSettingsLogService } from '@/core/infrastructure/adapters/services/codeReviewSettingsLog/codeReviewSettingsLog.service';
+import { CODE_REVIEW_SETTINGS_LOG_REPOSITORY_TOKEN } from '@/core/domain/codeReviewSettingsLog/contracts/codeReviewSettingsLog.repository.contract';
+import { CODE_REVIEW_SETTINGS_LOG_SERVICE_TOKEN } from '@/core/domain/codeReviewSettingsLog/contracts/codeReviewSettingsLog.service.contract';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [MongooseModule.forFeature([CodeReviewSettingsLogModelInstance])],
+    providers: [
+        {
+            provide: CODE_REVIEW_SETTINGS_LOG_REPOSITORY_TOKEN,
+            useClass: CodeReviewSettingsLogRepository,
+        },
+        {
+            provide: CODE_REVIEW_SETTINGS_LOG_SERVICE_TOKEN,
+            useClass: CodeReviewSettingsLogService,
+        },
+    ],
+    exports: [
+        CODE_REVIEW_SETTINGS_LOG_REPOSITORY_TOKEN,
+        CODE_REVIEW_SETTINGS_LOG_SERVICE_TOKEN,
+    ],
+})
+export class CodeReviewSettingsLogModule {}
