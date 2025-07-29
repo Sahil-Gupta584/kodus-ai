3776a98b2fb054f943b2823cb5961a6b0ea69b4b
malinosqui <gabrielmalinosqui@gmail.com>
2025-07-28 22:40:24 -0300

    adding pinologger error for code review log

--- src/core/application/use-cases/user/register-user-status-log.use-case.ts
+++ src/core/application/use-cases/user/register-user-status-log.use-case.ts
@@ -3,38 +3,51 @@ import { IUseCase } from '@/shared/domain/interfaces/use-case.interface';
 import { UserStatusDto } from '@/core/infrastructure/http/dtos/user-status-change.dto';
 import {
     CODE_REVIEW_SETTINGS_LOG_SERVICE_TOKEN,
-    ICodeReviewSettingsLogService
+    ICodeReviewSettingsLogService,
 } from '@/core/domain/codeReviewSettingsLog/contracts/codeReviewSettingsLog.service.contract';
 import { ActionType } from '@/config/types/general/codeReviewSettingsLog.type';
+import { PinoLoggerService } from '@/core/infrastructure/adapters/services/logger/pino.service';
 
 @Injectable()
 export class RegisterUserStatusLogUseCase implements IUseCase {
     constructor(
         @Inject(CODE_REVIEW_SETTINGS_LOG_SERVICE_TOKEN)
         private readonly codeReviewSettingsLogService: ICodeReviewSettingsLogService,
+        private readonly logger: PinoLoggerService,
     ) {}
 
     public async execute(userStatusDto: UserStatusDto): Promise<void> {
-        const organizationId = userStatusDto.organizationId;
+        try {
+            const organizationId = userStatusDto.organizationId;
 
-        await this.codeReviewSettingsLogService.registerUserStatusLog({
-            organizationAndTeamData: {
-                organizationId,
-                teamId: userStatusDto.teamId || null,
-            },
-            userInfo: {
-                userId: userStatusDto.editedBy.userId || '',
-                userEmail: userStatusDto.editedBy.email || '',
-            },
-            userStatusChanges: [
-                {
-                    gitId: userStatusDto.gitId,
-                    gitTool: userStatusDto.gitTool,
-                    userName: userStatusDto.userName,
-                    licenseStatus: userStatusDto.licenseStatus === 'active',
+            await this.codeReviewSettingsLogService.registerUserStatusLog({
+                organizationAndTeamData: {
+                    organizationId,
+                    teamId: userStatusDto.teamId || null,
                 },
-            ],
-            actionType: ActionType.EDIT,
-        });
+                userInfo: {
+                    userId: userStatusDto.editedBy.userId || '',
+                    userEmail: userStatusDto.editedBy.email || '',
+                },
+                userStatusChanges: [
+                    {
+                        gitId: userStatusDto.gitId,
+                        gitTool: userStatusDto.gitTool,
+                        userName: userStatusDto.userName,
+                        licenseStatus: userStatusDto.licenseStatus === 'active',
+                    },
+                ],
+                actionType: ActionType.EDIT,
+            });
+        } catch (error) {
+            this.logger.error({
+                message: 'Error registering user status log',
+                context: RegisterUserStatusLogUseCase.name,
+                error: error,
+                metadata: {
+                    ...userStatusDto,
+                },
+            });
+        }
     }
 }
