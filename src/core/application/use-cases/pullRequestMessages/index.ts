c65e3780213296e136b7eba3653317453163eb5d
Junior Sartori <sartorijr92@gmail.com>
2025-08-09 13:59:16 -0300

    Fixing get pr message by directory

--- src/core/application/use-cases/pullRequestMessages/index.ts
+++ src/core/application/use-cases/pullRequestMessages/index.ts
@@ -1,9 +1,11 @@
 import { CreateOrUpdatePullRequestMessagesUseCase } from './create-or-update-pull-request-messages.use-case';
 import { FindByRepositoryIdPullRequestMessagesUseCase } from './find-by-repository-id.use-case';
 import { FindByIdPullRequestMessagesUseCase } from './find-by-id.use-case';
+import { FindByDirectoryIdPullRequestMessagesUseCase } from './find-by-directory-id.use-case';
 
 export const PullRequestMessagesUseCases = [
     CreateOrUpdatePullRequestMessagesUseCase,
     FindByRepositoryIdPullRequestMessagesUseCase,
     FindByIdPullRequestMessagesUseCase,
+    FindByDirectoryIdPullRequestMessagesUseCase,
 ];
--- src/core/infrastructure/http/controllers/pullRequestMessages.controller.ts
+++ src/core/infrastructure/http/controllers/pullRequestMessages.controller.ts
@@ -1,6 +1,7 @@
 import { CreateOrUpdatePullRequestMessagesUseCase } from '@/core/application/use-cases/pullRequestMessages/create-or-update-pull-request-messages.use-case';
 import { FindByRepositoryIdPullRequestMessagesUseCase } from '@/core/application/use-cases/pullRequestMessages/find-by-repository-id.use-case';
 import { FindByIdPullRequestMessagesUseCase } from '@/core/application/use-cases/pullRequestMessages/find-by-id.use-case';
+import { FindByDirectoryIdPullRequestMessagesUseCase } from '@/core/application/use-cases/pullRequestMessages/find-by-directory-id.use-case';
 import { IPullRequestMessages } from '@/core/domain/pullRequestMessages/interfaces/pullRequestMessages.interface';
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
@@ -10,6 +11,7 @@ export class PullRequestMessagesController {
         private readonly createOrUpdatePullRequestMessagesUseCase: CreateOrUpdatePullRequestMessagesUseCase,
         private readonly findByRepositoryIdPullRequestMessagesUseCase: FindByRepositoryIdPullRequestMessagesUseCase,
         private readonly findByIdPullRequestMessagesUseCase: FindByIdPullRequestMessagesUseCase,
+        private readonly findByDirectoryIdPullRequestMessagesUseCase: FindByDirectoryIdPullRequestMessagesUseCase,
     ) {}
 
     @Post('/')
@@ -28,6 +30,17 @@ export class PullRequestMessagesController {
     ) {
         return await this.findByRepositoryIdPullRequestMessagesUseCase.execute(
             repositoryId,
+            organizationId,
+        );
+    }
+
+    @Get('/find-by-directory-id')
+    public async findByDirectoryId(
+        @Query('directoryId') directoryId: string,
+        @Query('organizationId') organizationId: string,
+    ) {
+        return await this.findByDirectoryIdPullRequestMessagesUseCase.execute(
+            directoryId,
             organizationId,
         );
     }
